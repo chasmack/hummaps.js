0a3fcfed4bb1d478b8ab6b84b4de14298668ed51
Charlie Mack <charlie@cmack.net>
2018-03-10 11:37:10 -0800

    refactor search

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'hummaps',
-  host: 'localhost',
+  host: 'hummaps.com',
   database: 'production',
   password: null,
   port: 5432,
--- routes/search.js
+++ routes/search.js
@@ -1,57 +1,53 @@
 'use strict';
 
 const router = require('express').Router();
-const searchQuery = require('../lib/search-query');
-const db = require('../db');
+const doSearch = require('../lib/search');
 
 router.get('/', (req, res, next) => {
-  db.query(searchQuery(req.query))
-    .then(results => {
-      results.rows.forEach(item => {
+  doSearch(req.query)
+  .then(mapRecs => {
 
-        // Replace structured arrays with objects
-        // item.ccs = ccs(item);
-        item.surveyors = surveyors(item);
+    // mapRecs.forEach(rec => {
 
-        // Composite attributes
-        item.bookpage = bookpage(item);
-        item.heading = heading(item);
-        item.line1 = line1(item);
-        item.line2 = line2(item);
-        item.line3 = line3(item);
-        item.line4 = line4(item);
-      });
+    //   // Composite attributes
+    //   rec.bookpage = bookpage(rec);
+    //   rec.heading = heading(rec);
+    //   rec.line1 = line1(rec);
+    //   rec.line2 = line2(rec);
+    //   rec.line3 = line3(rec);
+    //   rec.line4 = line4(rec);
+    // });
 
-      res.render('search', {
-        title: 'Search Results',
-        results: results,
-        json: JSON.stringify(results.rows, null, 2)
-      });
-    })
-    .catch(next);
+    res.render('search', {
+      title: 'Search Results',
+      count: mapRecs.length,
+      json: JSON.stringify(mapRecs, null, 2)
+    });
+  })
+  .catch(next);
 });
 
-function bookpage(item) {
-  return `${item.book}${item.abbrev}${item.page}`;
+function bookpage(rec) {
+  return `${rec.book}${rec.abbrev}${rec.page}`;
 }
 
-function heading(item) {
-  let heading = `${item.book} ${item.maptype}s ${item.page}`;
-  if (item.npages > 1) {
-    heading += `-${item.page + item.npages -1}`;
+function heading(rec) {
+  let heading = `${rec.book} ${rec.maptype}s ${rec.page}`;
+  if (rec.npages > 1) {
+    heading += `-${rec.page + rec.npages -1}`;
   }
   return heading;
 }
 
-function line1(item) {
-  return item.surveyors.length ?
-    `By: ${item.surveyors.map(s => s.fullname).join(', ')}` :
+function line1(rec) {
+  return rec.surveyors.length ?
+    `By: ${rec.surveyors.map(s => s.fullname).join(', ')}` :
     'By: (UNKNOWN)';
 }
 
-function line2(item) {
+function line2(rec) {
   let line2;
-  const date = item.recdate;
+  const date = rec.recdate;
   if (date) {
     line2 = `Rec: ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   } else {
@@ -60,37 +56,12 @@ function line2(item) {
   return line2;
 }
 
-function line3(item) {
-    return item.client ? `For: ${item.client}` : null;
+function line3(rec) {
+    return rec.client ? `For: ${rec.client}` : null;
 }
 
-function line4(item) {
-    return `Desc: ${item.description}`;
-}
-
-function surveyors(item) {
-  const surveyors = [];
-  item.surveyors.forEach((surveyor) => {
-    //const [firstname, secondname, thirdname, lastname, suffix, pls, rce] = surveyor;
-
-    let fullname = surveyor.slice(0,1);
-    surveyor.slice(1,3).forEach(name => {if (name) fullname.push(name.slice(0,1));});
-    surveyor.slice(3,5).forEach(name => {if (name) fullname.push(name);});
-    fullname = fullname.join(' ').toLowerCase().replace(/\b\w/g, c => c.toUpperCase());
-
-    let licenses = [];
-    const [pls, rce] = surveyor.slice(5,7);
-    if (pls) licenses.push('LS' + pls);
-    if (rce) licenses.push('RCE' + rce);
-    if (licenses.length) fullname += ` (${licenses.join(', ')})`;
-
-    surveyors.push({
-      fullname: fullname,
-      firstname: surveyor[0],
-      lastname: surveyor[3],
-    });
-  });
-  return surveyors;
+function line4(rec) {
+    return `Desc: ${rec.description}`;
 }
 
 module.exports = router;
